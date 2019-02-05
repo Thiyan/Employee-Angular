1a9c88f37ac87313ba52a90d5b46e9f96683dcae
Thiyan <thiyaneng@gmail.com>
2019-02-05 15:13:15 +0530

    reply

--- src/app/employee/reply/reply.component.ts
+++ src/app/employee/reply/reply.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ReplyService } from './reply.service';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-reply',
@@ -8,53 +9,78 @@ import { ReplyService } from './reply.service';
 })
 export class ReplyComponent implements OnInit {
 
-  replies:any[];
-  requests:any[];
-  constructor(private service:ReplyService) { }
-  
+  replies: any[];
+  reply;
+  requests: any[];
+  constructor(private service: ReplyService) { }
+
   ngOnInit() {
 
     this.service.getAllRequests()
-    .subscribe(response=>{
-      if(JSON.parse(JSON.stringify(response)).statusCode==="S1000"){
-        this.requests=JSON.parse(JSON.stringify(response))['content'];
+    .subscribe(response => {
+      if (JSON.parse(JSON.stringify(response)).statusCode === 'S1000') {
+        this.requests = JSON.parse(JSON.stringify(response))['content'];
         console.log(this.requests);
-      }
-      else{
+      } else {
         // alert(JSON.parse(JSON.stringify(response)).statusDescription);
-        if(JSON.parse(JSON.stringify(response)).statusCode==="E1003"){
-          alert("No results Found");
-        }
-        else{
-          alert("An unexpected error occurred");
+        if (JSON.parse(JSON.stringify(response)).statusCode === 'E1003') {
+          alert('No results Found');
+        } else {
+          alert('An unexpected error occurred');
         }
       }
-    },error=>{
-        alert("An unexpected error occurred");
+    }, error => {
+        alert('An unexpected error occurred');
     }
 );
-    
+
 
 this.service.getReplies()
-    .subscribe(response=>{
-      if(JSON.parse(JSON.stringify(response)).statusCode==="S1000"){
-        this.replies=JSON.parse(JSON.stringify(response))['content'];
+    .subscribe(response => {
+      if (JSON.parse(JSON.stringify(response)).statusCode === 'S1000') {
+        this.replies = JSON.parse(JSON.stringify(response))['content'];
         console.log(this.replies);
-      }
-      else{
+      } else {
         // alert(JSON.parse(JSON.stringify(response)).statusDescription);
-        if(JSON.parse(JSON.stringify(response)).statusCode==="E1003"){
-          alert("No results Found");
-        }
-        else{
-          alert("An unexpected error occurred");
+        if (JSON.parse(JSON.stringify(response)).statusCode === 'E1003') {
+          alert('No results Found');
+        } else {
+          alert('An unexpected error occurred');
         }
       }
-    },error=>{
-        alert("An unexpected error occurred");
+    }, error => {
+        alert('An unexpected error occurred');
     }
 );
+  }
 
+  giveReply(reply) {
+
+    console.log(reply);
+    this.reply = reply;
+  }
+
+  onsubmit(f: NgForm) {
+
+    console.log(f.value);
+
+    const reply = {
+
+      'requestId': this.reply.requestId,
+      'requestedBy': this.reply.requestedBy,
+      'requestedDate': this.reply.requestedDate,
+      'date': this.reply.date,
+      'reason': this.reply.reason,
+      'checked': true,
+      'approvedBy': {'eId': 9},
+      'approved': f.value.approved,
+      'reply': f.value.reply,
+      'valid': this.reply.valid
+    };
+
+    console.log(reply);
+
+    this.service.addReply(reply);
   }
 
 }
--- src/app/employee/reply/reply.service.ts
+++ src/app/employee/reply/reply.service.ts
@@ -1,30 +1,60 @@
-import { HttpClient } from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import {NgForm} from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReplyService {
 
-  url="http://localhost:8080/manager/requests";
-  url2="http://localhost:8080/manager/my-reply"
-  ;
+  url = 'http://localhost:8080/manager/requests';
+  url2 = 'http://localhost:8080/manager/my-reply';
+  url3 = 'http://localhost:8080/manager/add-reply';
 
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-  getReplies():Observable<Object>{
-    
-    return this.http.get(this.url2,{
-      params:{
-        id:"Arumugam201"
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      // 'Authorization':'626fbc3a-536e-4c4b-aec7-38299aa0957a'
+    })
+  };
+
+  getReplies(): Observable<Object> {
+
+    return this.http.get(this.url2, {
+      params: {
+        id: 'Arumugam201'
       }});
-        
+
   }
 
-  getAllRequests():Observable<Object>{
-    
+  getAllRequests(): Observable<Object> {
+
     return this.http.get(this.url);
-        
+
+  }
+
+
+
+  addReply(reply ) {
+
+    this.http.put(this.url3, reply, this.httpOptions)
+      .subscribe(response => {
+
+        console.log(response);
+        if (JSON.parse(JSON.stringify(response)).statusCode === 'S1000') {
+          alert('Reply added successfully!');
+        } else {
+          alert(JSON.parse(JSON.stringify(response)).statusDescription);
+        }
+        console.log(response);
+      }, error => {
+        alert('An unexpected error occurred');
+
+      });
+
   }
 }
